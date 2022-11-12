f145dd498efb5a47782134301acb28f6fe0386ed
Marta-asturias <marta.gonzalez@domoticadavinci.com>
2022-11-12 01:20:36 +0100

    finish

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-ALTER TABLE product AUTO_INCREMENT = 1;
+/* ALTER TABLE product AUTO_INCREMENT = 1;
 
 INSERT INTO product (cantidad,name,referencia) values ("10","panes","0100");
 INSERT INTO product (cantidad,name,referencia) values ("20","pasteles","0101");
-INSERT INTO product (cantidad,name,referencia) values ("20","empanadas","0102");
+INSERT INTO product (cantidad,name,referencia) values ("20","empanadas","0102"); */
